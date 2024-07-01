abf537b23987b795734c17f9e0461010b022b308
Harsh Trivedi <harshtrivedi@Harshs-MacBook-Air.local>
2024-07-01 10:46:02 -0700

    11th commit

---
+++ 1_loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#this is demo script for for loop :xd
+
+echo "###################################"
+for var1 in java .net python ruby php scripting
+do 
+    echo "looping ................"
+    sleep 1
+    echo "######################################"
+    echo "value of var1 is $var1"
+    echo "######################################"
+    date
+done
